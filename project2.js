71e6d469ee7668a8dcac3f196bf12f60dabf2874
Tirth <tirthvshah7@gmail.com>
2020-05-04 15:37:47 +0530

    object

---
+++ project2.js
@@ -0,0 +1,6 @@
+function generate(){
+    var image=document.createElement('img');
+    var div = document.getElementById('flexBoxGen')
+    image.src="download.jpg";
+    div.appendChild(image);
+}
